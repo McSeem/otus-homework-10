28ec86343cd08586cd500b249ff9f7ba8a6b0033
Max Vetrov <maksim.vetrov@webpp.ru>
2023-05-31 15:24:46 +0300

    Исправления линтера

--- djangoapp/lmsbaikal/views.py
+++ djangoapp/lmsbaikal/views.py
@@ -16,21 +16,21 @@ class QuestionDetailView(DetailView):
     model = Question
 
 
-class QuestionNewView(CreateView): # pylint: disable=too-many-ancestors
+class QuestionNewView(CreateView): # pylint: disable=too-many-ancestors,too-few-public-methods
     """ CBV-представление страницы с формой добавления вопроса. """
     model = Question
     fields = "__all__"
     success_url = "/lmsbaikal/"
 
 
-class QuestionUpdateView(UpdateView): # pylint: disable=too-many-ancestors
+class QuestionUpdateView(UpdateView): # pylint: disable=too-many-ancestors,too-few-public-methods
     """ CBV-представление страницы с формой редактирования вопроса. """
     model = Question
     fields = "__all__"
     success_url = "/lmsbaikal/"
 
 
-class QuestionAnswersAddView(CreateView): # pylint: disable=too-many-ancestors
+class QuestionAnswersAddView(CreateView): # pylint: disable=too-many-ancestors,too-few-public-methods
     """ CBV-представление страницы с формой добавления вариантов ответов для вопроса. """
     model = QuestionAnswers
     fields = "__all__"
